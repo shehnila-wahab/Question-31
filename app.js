d2a8772e52b9b6d4d5a71fb4707cfadaea72c31a
shehnila-wahab <160872560+shehnila-wahab@users.noreply.github.com>
2024-05-25 23:33:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var user = ['sana', 'sahil', 'saba', 'shehnila', 'anam'];
+if (user.length) {
+    console.log("We need to find some users!");
+}
+else {
+    user = [];
+    console.log("all the users have been removed" + user.length);
+}
---
+++ app.ts
@@ -0,0 +1,8 @@
+let user:string[]= ['sana','sahil','saba','shehnila','anam']
+if(user.length===0){
+    console.log("We need to find some users!")
+}
+else{
+    user = [];
+    console.log("all the users have been removed"+user.length)
+}
